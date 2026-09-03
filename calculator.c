577817afcd165f2ff92738ab0ff6de2db7c980e5
Nadia Lafontant <lafontantnadia0@gmail.com>
2026-09-03 09:35:10 -0500

    Implemented addition feature

--- calculator.c
+++ calculator.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+double add (double a, double b) {
+    return (a + b);
+}
+
 int main() {
-    
+    double num1, num2;
+
+    printf("Enter two integers:");
+    scanf("%lf %lf", &num1, &num2);
+
+    double sum = num1 + num2;
+
+    printf("By addition, %lf + %lf = %lf\n", num1, num2, sum);
+    return 0;
 }
