9d9afb05c517c1787c2de865e551116f9d5e33d2
Nadia Lafontant <lafontantnadia0@gmail.com>
2026-09-03 09:38:47 -0500

    Implemented subtraction feature

--- calculator.c
+++ calculator.c
@@ -4,6 +4,10 @@ double add (double a, double b) {
     return (a + b);
 }
 
+double subtract (double a, double b) {
+    return (a - b);
+}
+
 int main() {
     double num1, num2;
 
@@ -11,7 +15,10 @@ int main() {
     scanf("%lf %lf", &num1, &num2);
 
     double sum = num1 + num2;
-
     printf("By addition, %lf + %lf = %lf\n", num1, num2, sum);
+
+    double difference = (num1 - num2);
+    printf("By subtraction, %lf - %lf = %lf\n", num1, num2, difference);
+
     return 0;
 }
