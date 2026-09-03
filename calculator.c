3b72da4df797c75520dd1121d705344f7d2ca96d
Nadia Lafontant <lafontantnadia0@gmail.com>
2026-09-03 09:43:22 -0500

    Implemented division feature

--- calculator.c
+++ calculator.c
@@ -12,20 +12,31 @@ double multiply (double a, double b) {
     return (a * b);
 }
 
+double divide (double a, double b) {
+    if (b == 0) {
+        printf("Error: Division by zero is not allowed.\n");
+        return 0;
+    }
+    return (a / b);
+}
+
 int main() {
     double num1, num2;
 
     printf("Enter two integers:");
     scanf("%lf %lf", &num1, &num2);
 
-    double sum = num1 + num2;
+    double sum = add(num1, num2);
     printf("By addition, %lf + %lf = %lf\n", num1, num2, sum);
 
-    double difference = (num1 - num2);
+    double difference = subtract(num1, num2);
     printf("By subtraction, %lf - %lf = %lf\n", num1, num2, difference);
 
-    double product = (num1 * num2);
+    double product = multiply(num1, num2);
     printf("By multiplication, %lf * %lf = %lf\n", num1, num2, product);
+
+    double quotient = divide(num1, num2);
+    printf("By division, %lf / %lf = %lf\n", num1, num2, quotient);
 
     return 0;
 }
